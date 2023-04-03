05c2572e71751c95830fed1ce15de6c2f13b9ef7
Jack Williams <jack.williams@fracturesounds.com>
2023-04-03 01:32:50 +0100

    Fixed get_ui_id() being added incorrectly for control_set_par when not PARENT_PANEL

--- compiler/ksp_compiler.py
+++ compiler/ksp_compiler.py
@@ -1521,8 +1521,8 @@ class ASTModifierFixPrefixesAndFixControlPars(ASTModifierFixPrefixes):
            len(node.parameters) > 0 and isinstance(node.parameters[0], ksp_ast.VarRef) and str(node.parameters[0].identifier).lower() in ui_variables:
             # then wrap the UI variable in a get_ui_id call, eg. myknob is converted into get_ui_id(myknob)
             func_call_inner = ksp_ast.FunctionCall(node.lexinfo, ksp_ast.ID(node.parameters[0].lexinfo, 'get_ui_id'), [node.parameters[0]], is_procedure=False)
-            # If last parameter is a ui_variable then add get_ui_id() wrapper
-            if function_name.startswith('set_control_par') and isinstance(node.parameters[2], ksp_ast.VarRef) and str(node.parameters[2].identifier).lower() in ui_variables:
+            # If last parameter is a ui_variable and is trying to add to parent_panel then add get_ui_id() wrapper
+            if function_name.startswith('set_control_par') and isinstance(node.parameters[2], ksp_ast.VarRef) and str(node.parameters[1]) == "$CONTROL_PAR_PARENT_PANEL" and str(node.parameters[2].identifier).lower() in ui_variables:
                 func_call_outer = ksp_ast.FunctionCall(node.lexinfo, ksp_ast.ID(node.parameters[2].lexinfo, 'get_ui_id'), [node.parameters[2]], is_procedure=False)
                 node.parameters[2] = func_call_outer
             node            = ksp_ast.FunctionCall(node.lexinfo, node.function_name, [func_call_inner] + node.parameters[1:], is_procedure=node.is_procedure)
