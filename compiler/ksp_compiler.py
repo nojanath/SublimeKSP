23f6c1123d91dab52340b5622db202a67daeb3ad
EvilDragon <mario.kruselj@gmail.com>
2025-01-24 18:36:39 +0100

    Fix missing regex signifiers in handle_conditional_lines() (#477)

--- compiler/ksp_compiler.py
+++ compiler/ksp_compiler.py
@@ -595,7 +595,7 @@ def handle_conditional_lines(lines):
             clear_this_line = True
 
         if not clear_this_line and 'SET_CONDITION(' in line:
-            m = re.search('\((.+?)\)', line)
+            m = re.search(r'\((.+?)\)', line)
 
             if m:
                 cond = m.group(1).strip()
@@ -613,7 +613,7 @@ def handle_conditional_lines(lines):
                         clear_this_line = True
 
         if 'USE_CODE_IF' in line:
-            m = re.search('\((.+?)\)', line)
+            m = re.search(r'\((.+?)\)', line)
 
             if m:
                 cond = m.group(1).strip()
