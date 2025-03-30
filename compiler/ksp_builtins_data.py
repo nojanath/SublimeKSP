d07036a471fcbcdd853e8009d4f66865a3472752
Mario Kruselj <mario.kruselj@gmail.com>
2025-03-30 22:08:30 +0200

    Add START_INC, END_INC, concat(), list_add() to builtins and reingest

--- compiler/ksp_builtins_data.py
+++ compiler/ksp_builtins_data.py
@@ -1559,6 +1559,8 @@ RESET_CONDITION(<condition>)
 SET_CONDITION(<condition>)
 USE_CODE_IF(<condition>)
 USE_CODE_IF_NOT(<condition>)
+START_INC(<text>, <from>, <to>)
+END_INC
 _get_engine_par(<parameter>, <group>, <slot>, <generic>):integer
 _get_engine_par_disp(<parameter>, <group>, <slot>, <generic>):string
 _get_folder(<file-path>):string
@@ -1601,6 +1603,14 @@ change_tune(<event-id>, <tune-amount>, <relative-bit>)
 change_velo(<event-id>, <new-velocity-number>)
 change_vol(<event-id>, <volume>, <relative-bit>)
 cbrt(<real-value>):real
+concat(<array1>):array
+concat(<array1>, <array2>):array
+concat(<array1>, <array2>, <array3>):array
+concat(<array1>, <array2>, <array3>, <array4>):array
+concat(<array1>, <array2>, <array3>, <array4>, <array5>):array
+concat(<array1>, <array2>, <array3>, <array4>, <array5>, <array6>):array
+concat(<array1>, <array2>, <array3>, <array4>, <array5>, <array6>, <array7>):array
+concat(<array1>, <array2>, <array3>, <array4>, <array5>, <array6>, <array7>, <array8>):array
 continue
 cos(<real-value>):real
 dec(<expression>):integer
@@ -1692,6 +1702,7 @@ int_to_real(<integer>):real
 is_zone_empty(<zone-id>):integer
 iterate_macro(<expression>)
 iterate_post_macro(<expression>)
+list_add(<list>, <value>)
 literate_macro(<expression>)
 literate_post_macro(<expression>)
 load_array(<any-array-variable>, <mode>):integer
@@ -1876,9 +1887,9 @@ ticks_to_ms(<ticks>):integer
 unload_slot(<instr-slot-idx>)
 wait(<wait-time>)
 wait_async(<event-id>):integer
+wait_ticks(<wait-time>)
 watch_var(<variable>)
 watch_array_idx(<any-array-variable>, <index>)
-wait_ticks(<wait-time>)
 will_never_terminate(<event-id>)
 zone_slice_idx_loop_end(<zone-id>, <loop-idx>):integer
 zone_slice_idx_loop_start(<zone-id>, <loop-idx>):integer
